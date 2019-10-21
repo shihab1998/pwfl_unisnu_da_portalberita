378716df3926891223b36073cf66158357308134
muhammad shihab kafibah <161240000491@unisnu.ac.id>
2019-10-21 14:55:47 +0700

    Template
    Mengubah template dan membuat menu baru

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -5,6 +5,14 @@ class Dashboard extends CI_Controller
 {
 	public function index()
 	{
-			$this->load->view('dashboard_v');
+		$this->load->view('Hal_login');
 	}
+	public function dashboardku()
+	{
+		$this->load->view('header');
+		$this->load->view('sidebar');
+		$this->load->view('dashboard_v');
+		$this->load->view('footer');
+	}
+
 }
---
+++ application/views/sidebar.php
@@ -0,0 +1,59 @@
+
+  <!-- Left side column. contains the logo and sidebar -->
+  <aside class="main-sidebar">
+
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+
+      <!-- Sidebar user panel (optional) -->
+      <div class="user-panel">
+        <div class="pull-left image">
+          <img src="<?php echo base_url() ?>assets/dist/img/api b.jpg" class="img-circle" alt="User Image">
+        </div>
+        <div class="pull-left info">
+          <p>Shihab</p>
+          <!-- Status -->
+          <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+        </div>
+      </div>
+
+      <!-- search form (Optional) -->
+      <form action="#" method="get" class="sidebar-form">
+        <div class="input-group">
+          <input type="text" name="q" class="form-control" placeholder="Search...">
+          <span class="input-group-btn">
+              <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
+              </button>
+            </span>
+        </div>
+      </form>
+      <!-- /.search form -->
+
+      <!-- Sidebar Menu -->
+      <ul class="sidebar-menu" data-widget="tree">
+        <li class="header"></li>
+        <!-- Optionally, you can add icons to the links -->
+        <li class="active"><a href="#"><i class="fa fa-link"></i> <span>Dashboard</span></a></li>
+        <li ><a href="#"><i class="fa fa-link"></i> <span>News</span></a></li>
+        <li><a href="#"><i class="fa fa-link"></i> <span>Lihat Berita</span></a></li>
+        <li><a href="#"><i class="fa fa-link"></i> <span>Tambah Berita</span></a></li>
+
+        <li class="treeview">
+          <a href="#"><i class="fa fa-link"></i> <span>Kategori Berita</span>
+            <span class="pull-right-container">
+                <i class="fa fa-angle-left pull-right"></i>
+              </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="#">Travel</a></li>
+            <li><a href="#">Sport</a></li>
+            <li><a href="#">Entertiment</a></li>
+            <li><a href="#">Otomotif</a></li>
+            <li><a href="#">Techno</a></li>            
+          </ul>
+        </li>
+      </ul>
+      <!-- /.sidebar-menu -->
+    </section>
+    <!-- /.sidebar -->
+  </aside>
